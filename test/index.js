c9ddb62f0296eeac08d613539a8413888aab9f8b
Jesús Leganés-Combarro 'piranna <piranna@gmail.com>
2022-06-17 09:56:28 +0200

    layout test

---
+++ test/index.js
@@ -0,0 +1,14 @@
+const {expect, use} = require('chai');
+const {test} = require('mocha');
+const { jestSnapshotPlugin } = require('mocha-chai-jest-snapshot');
+
+const plugin = require('../lib/index');
+
+
+use(jestSnapshotPlugin());
+
+
+test('layout', function()
+{
+  expect(plugin).toMatchSnapshot();
+})
